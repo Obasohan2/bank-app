dd22a44bd998d03e6c51d8ab487d2c97ec95a80c
Dreams Obasohan <jtp7d03u@students.codeinstitute.net>
2025-01-01 09:41:22 +0000

    Create access to worksheet account

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
+from prettytable import PrettyTable
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +14,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('bank_app')
 
-sales = SHEET.worksheet('sales')
+# Access the sheet
+accounts_sheet = SHEET.worksheet("accounts")
 
-data = sales.get_all_values()
+from prettytable import PrettyTable
 
-print(data)
+def print_database():
+    """Print all accounts in the database in a formatted table."""
+    accounts = accounts_sheet.get_all_records()
+    if not accounts:
+        print("No accounts found in the database.")
+        return
